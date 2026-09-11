bea8b3fdd5a459ae809e4ee1da750681d90ce426
unknown <j.d.kotlarski@gmx.com>
2026-09-11 15:57:55 +0200

    Cannot import axios?

--- resources/js/domains/books/pages/OverView.vue
+++ resources/js/domains/books/pages/OverView.vue
@@ -1,1 +1,20 @@
-<template></template>
+<script>
+import { onMounted } from 'vue';
+import { getAllBooks, fetchBooks } from '../store';
+
+fetchBooks();
+getAllBooks();
+</script>
+
+<template>
+    <table>
+        <tr>
+            <th>Title</th>
+            <th>Summary</th>
+        </tr>
+        <tr v-for="book in getAllBooks" :key="book.id">
+            <td>{{ book.title }}</td>
+            <td>{{ book.summary }}</td>
+        </tr>
+    </table>
+</template>
