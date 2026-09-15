7e0cae9235d53bbef9de9bb394e74df7b658e820
janderkkotlarski <j.d.kotlarski@gmx.com>
2026-09-15 12:36:56 +0200

    the servers seem to be working

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,4 @@
 <template>
-    <nav>
-        <router-link :to="{name: 'books.overview'}">Boeken Overzicht</router-link> |
-        <router-link :to="{name: 'books.create'}">Nieuw Boek</router-link>
-    </nav>
-    <router-view></router-view>
+Hello
+    
 </template>
