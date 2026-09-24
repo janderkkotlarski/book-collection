bfd83b76505c041c23a56f7c74c8256b6eb0b180
unknown <j.d.kotlarski@gmx.com>
2026-09-24 15:02:05 +0200

    incorporate hasFactory in order for the factory to be utilized

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Author extends Model
-{
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Author extends Model {
+    protected $fillable = ['name'];
+
+    use HasFactory;
+
     public function books() {
         return $this->hasMany(Book::class);
     }
